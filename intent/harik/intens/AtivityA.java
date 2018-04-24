a251a1864e4efa289614f94a347ce7fa74336a78
Harik med <hamouda.form@gmail.com>
2018-04-24 11:25:29 +0000

    les intents ... explicites

---
+++ intent/harik/intens/AtivityA.java
@@ -0,0 +1,37 @@
+package com.harik.intens;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.harik.lenovo2017.dawmrappel.R;
+
+public class AtivityA extends Activity {
+EditText edprix, edqte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ativity);
+        edprix= (EditText) findViewById(R.id.edprix);
+        edqte= (EditText) findViewById(R.id.edqte);
+
+    }
+    
+    public void calculer(View v){
+        // recuprer les infos ds les editexts
+        String prix=edprix.getText().toString();
+        String qte = edqte.getText().toString();
+        //on crée l'intent explicite permettant de démarrer de a à b
+        Intent i=new Intent(AtivityA.this,ActivityB.class);
+        //on enregistre des données dans l'intent i
+        i.putExtra("prix",prix);
+        i.putExtra("qte",qte);
+
+// on lance l'activité on lui donnant l'intent i
+        startActivity(i);
+    }
+}
---
+++ intent/harik/lenovo2017/dawmrappel/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.harik.lenovo2017.dawmrappel;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+    }
+
+
+}
